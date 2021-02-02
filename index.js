e50f970ca5af3c3ac22c682dad6cb7358e3b8a0a
“Abbie <abbie.coghlan@gmail.com>
2021-02-02 08:46:42 -0600

    done

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
-    let right = 400 - left
-    if (right < 400) {
+     if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   } 
